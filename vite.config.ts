5f27a9d1f000be9befc1d783061d26476c07e364
squidy-main <veronultra1@gmail.com>
2025-04-15 20:39:49 +0530

    ci: update deployment configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/squidy-main.github.io/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -16,7 +16,7 @@ export default defineConfig({
     assetsDir: 'assets',
     rollupOptions: {
       input: {
-        app: './index.html'
+        main: path.resolve(__dirname, 'index.html'),
       }
     }
   },
